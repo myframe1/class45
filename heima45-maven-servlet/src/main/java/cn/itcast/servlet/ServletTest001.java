4d72fbb7fdfe6f9d895a5355cd52102d63ee3b5c
liuhaolong <liuhaolong_2@163.com>
2019-10-30 12:09:10 +0800

    提交测试1

---
+++ heima45-maven-servlet/src/main/java/cn/itcast/servlet/ServletTest001.java
@@ -0,0 +1,19 @@
+package cn.itcast.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ServletTest001")
+public class ServletTest001 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
